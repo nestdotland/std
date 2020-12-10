5809668a86fd8db521362d589607074b7919f3e2
Maximous Black <maximousblk@gmail.com>
2020-12-10 12:10:16 +0530

    Update update.ts
    revert this too

--- update.ts
+++ update.ts
@@ -1,22 +1,22 @@
-import * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+// import * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
 import { writeJson } from "https://x.nest.land/std@0.62.0/fs/mod.ts";
 
 // import { VERSION } from "https://github.com/denoland/deno/raw/master/std/version.ts";
 
 const VERSION: string = String(Deno.args[0])
 
-const nestAPI = "https://x.nest.land/api/package/std";
+// const nestAPI = "https://x.nest.land/api/package/std";
 
-const egg: any = await fetch(nestAPI)
-  .then((response) => response.json())
-  .then((data) => {
-    return semver.clean(data.latestVersion.replace("std@", ""));
-  });
+// const egg: any = await fetch(nestAPI)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     return semver.clean(data.latestVersion.replace("std@", ""));
+//   });
 
-console.log("nest.land: ", egg);
+// console.log("nest.land: ", egg);
 console.log("deno.land: ", VERSION);
 
-const diff: number = semver.compare(egg, VERSION);
+// const diff: number = semver.compare(egg, VERSION);
 
 const config: object = {
   "name": "std",
@@ -33,11 +33,11 @@ const config: object = {
   ],
 };
 
-if (diff === 0) {
-  // this ain't it chief!
-  console.log(`std@${egg} is already published`);
-  Deno.exit(0);
-} else {
+// if (diff === 0) {
+//   // this ain't it chief!
+//   // console.log(`std@${egg} is already published`);
+//   Deno.exit(0);
+// } else {
   console.log("New version found!\nCreating config...");
   await writeJson("egg.json", config, { spaces: 2 });
   console.log(`Publishing std@${VERSION}...`);
@@ -50,4 +50,4 @@ if (diff === 0) {
     ]
   });
   Deno.exit(0);
-}
+// }
